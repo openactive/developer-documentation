4ab93a43e5ecfd064d988e79b593fb836e380be1
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-08-09 16:05:17 +0100

    Fix

--- embed.js
+++
@@ -1,4 +0,0 @@
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js" type="text/javascript" charset="utf-8"></script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js" type="text/javascript" charset="utf-8"></script>');
-document.write('<div id="editor" style="width:100%;height:12em;">\nimport java.util.*;\nclass HW{\npublic static void main(String [] args){\nSystem.out.println("HW");}</div>');
-document.write('<script>var editor = ace.edit("editor"); editor.setTheme("ace/theme/monokai"); editor.getSession().setMode("ace/mode/java"); editor.setReadOnly(true); editor.setShowPrintMargin(false);</script>');
