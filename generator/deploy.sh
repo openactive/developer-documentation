724c5cfc706730754bcf4f98e85f67deeafdbbe9
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-10-08 18:58:14 +0100

    Improved model doc gen script

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -7,11 +7,11 @@ setup_git() {
 
 commit_generated_files() {
   git status
-  git add ../docs/data-model/types/
+  git add ../docs/data-model/types/*
   git commit --message "Updating auto-generated model documentation: $TRAVIS_BUILD_NUMBER [ci skip]"
 }
 
-upload_files() {
+push_files() {
   git remote add origin-master https://${GH_TOKEN}@github.com/openactive/developer-documentation.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-master master
 }
@@ -26,5 +26,5 @@ echo "Git status:"
 git status
 
 setup_git
-commit_website_files
-upload_files
+commit_generated_files
+push_files
