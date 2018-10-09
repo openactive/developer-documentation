739f9774d91a5d79b6caf6484f5836dfaad00873
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-10-09 10:28:57 +0100

    [TEST] Minor tweaks to script (#12)
    * Minor tweaks
    * Recreate draft branch instead of force pushing

--- reset-draft.sh
+++ reset-draft.sh
@@ -3,6 +3,7 @@
 git checkout master
 git pull
 git branch -D draft
+git push origin --delete draft
 git checkout -b draft
-git push --force -u origin draft
+git push -u origin draft
 git checkout master
