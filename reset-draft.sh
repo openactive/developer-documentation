aa87af7044215ee6c0d9ed7f00bfb9cc350b2c1b
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-10-09 10:25:01 +0100

    Recreate draft branch instead of force pushing

--- reset-draft.sh
+++ reset-draft.sh
@@ -3,7 +3,7 @@
 git checkout master
 git pull
 git branch -D draft
-git push origin --delete docs/test
+git push origin --delete draft
 git checkout -b draft
 git push -u origin draft
 git checkout master
